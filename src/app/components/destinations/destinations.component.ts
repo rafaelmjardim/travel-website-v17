8dc1244e0d083fdef2186695e5b0b06c323739ed
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-11-12 21:23:52 -0300

    refatorado destinations

--- src/app/components/destinations/destinations.component.ts
+++ src/app/components/destinations/destinations.component.ts
@@ -10,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class DestinationsComponent {
 
+  cards: {title: string, price: string, date: string, img: string}[] = [
+    {
+      title: "RouterModule, Italy",
+      price: "$5,32k",
+      date: "10 Days Trip",
+      img: "/assets/Rome.png"      
+    },
+    {
+      title: "London, UK",
+      price: "$4,2k",
+      date: "12 Days Trip",
+      img: "/assets/London.png"      
+    },
+    {
+      title: "Full Europe",
+      price: "$15k",
+      date: "28 Days Trip",
+      img: "/assets/Europe.png"      
+    },
+  ];
+
 }
